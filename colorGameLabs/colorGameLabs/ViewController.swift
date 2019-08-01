09b7311431dac8dcede674124d030b0daca281ad
OliveiraAlbertCoelho <albertoliveira@pursuit.org>
2019-07-31 21:52:46 -0400

    kinda done

--- colorGameLabs/colorGameLabs/ViewController.swift
+++ colorGameLabs/colorGameLabs/ViewController.swift
@@ -10,57 +10,84 @@ import UIKit
 
 class ViewController: UIViewController {
     override func viewDidLoad() {
+        playGameOut.isHidden = true
+        
         super.viewDidLoad()
         gameColor.backgroundColor = game.giveRandomUIColor()
+        playGameOut.layer.borderColor = UIColor.black.cgColor;
+        playGameOut.layer.borderWidth = 10.0;
     }
-   var game = RGBColors()
+    var game = RGBColors()
     var counter = 0
     var highScore = 0
     @IBOutlet weak var score: UILabel!
     @IBOutlet weak var gameColor: UIView!
     @IBOutlet weak var highScoreLabel: UILabel!
+    @IBOutlet weak var redOut: UIButton!
+    @IBOutlet weak var blueOut: UIButton!
+    @IBOutlet weak var greenOut: UIButton!
+    @IBOutlet weak var playGameOut: UIButton!
     
+    
+    @IBAction func playAgainMsg(_ sender: UIButton) {
+        changeColor()
+        disableStuff()
+        playGameOut.isHidden = true
+        blueOut.isEnabled = true
+        redOut.isEnabled = true
+        greenOut.isEnabled = true
+   
+    }
+   
     @IBAction func redButton(_ sender: UIButton) {
-        let red = "red"
-        if red == game.getBiggest(){
-           updateScore()
+        if "red" == game.getBiggest(){
+            updateScore()
             changeColor()
         } else {
+            disableStuff()
             getHighScore()
-                resetScore()
+            resetScore()
+            playGameOut.isHidden = false
+         
         }
     }
     
     @IBAction func greenButton(_ sender: UIButton) {
-        let green = "green"
-        if green == game.getBiggest(){
+        if "green" == game.getBiggest(){
             updateScore()
             changeColor()
-
+            
         }else {
+            disableStuff()
             getHighScore()
             resetScore()
+            playGameOut.isHidden = false
         }
-       
+        
     }
     
     
     
     @IBAction func blueButton(_ sender: UIButton) {
-        let blue = "blue"
-        if blue == game.getBiggest(){
+        if "blue" == game.getBiggest(){
             updateScore()
             changeColor()
             
-
+            
         }else {
+            disableStuff()
             getHighScore()
             resetScore()
+            playGameOut.isHidden = false
         }
     }
-    
+    func disableStuff(){
+        redOut.isEnabled = false
+        blueOut.isEnabled = false
+        greenOut.isEnabled = false
+    }
     func updateScore() {
-    
+        
         counter += 1
         score.text = "Score: \(counter.description)"
     }
@@ -71,9 +98,9 @@ class ViewController: UIViewController {
     func getHighScore(){
         if counter > highScore {
             highScore = counter
-             highScoreLabel.text = "High Score: \(highScore.description)"
+            highScoreLabel.text = "High Score: \(highScore.description)"
         }
-    
+        
     }
     func changeColor(){
         self.game = RGBColors()
